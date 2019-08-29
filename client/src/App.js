abc3549a0d3319c1939a13126280968649b3cd2d
Matias Fernandez <mfernandez@hexacta.com>
2019-08-29 01:19:20 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Cartas from "./Cartas";
+
+function App() {
+  return <Cartas />;
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 4000;
+var server = app.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
